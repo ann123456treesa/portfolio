d52d0e7d29bc3ae79a11382eafc05724f8dd26ee
Anil Peter R <anilpeter75@gmail.com>
2024-08-31 20:50:23 +0530

    initial commit

---
+++ portfolio.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ------ Header Section -----
+with st.container():
+    st.subheader("Hi i am Aleena :wave:")
+    st.title("3D Generalist")
+    st.write("I am a passionate 3D Artist ")
+    st.write("[Learn More >](https://pythonandvba.com)")
